b608265f5673fb55af4a1d24aace0f7ec089c0e0
Ramesh <rameshamale50@gmail.com>
2025-09-13 12:51:30 +0530

    Wrong password is uopdated with correct once

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", function(e){
     const password = document.getElementById("password").value;
     
     // Demo credentials
-    if(email === "rameshamale26@gmail.com" && password === "Test@123"){
+    if(email === "rameshamale26@gmail.com" && password === "Amale@123""){
         alert("Login successful!");
         window.location.href = "dashboard.html";
     } else {
